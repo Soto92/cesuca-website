f40d82ccea832325c7ecb3629145d64baed32d65
RodrigoTrps <rodrigo.estudo2023@gmail.com>
2024-06-17 20:25:30 -0300

    ajuste tela

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
             position: "absolute",
             top: "50%",
             left: "50%",
-            transform: "translate(-50%, 250px)",
+            transform: "translate(-50%, 100px)",
             zIndex: "2",
             width: "598px",
           }}
