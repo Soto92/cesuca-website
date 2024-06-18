7d6833d7e16f277d0fced2520b55fa5483c6f356
Mauricio <mauricioasoto@gmail.com>
2024-06-18 20:19:12 -0300

    add buttons in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,40 @@
+import InstaIMG from "../assets/images/insta.png";
+import HotmartIMG from "../assets/images/hotmart.png";
+
+const InstaButton = () => {
+  return (
+    <a target="_blank" href="https://www.instagram.com/ordem.financeira">
+      <img
+        src={InstaIMG}
+        alt="Insta"
+        style={{
+          height: "40px",
+          width: "auto",
+        }}
+      />
+    </a>
+  );
+};
+
+const HotmartButton = () => {
+  return (
+    <a
+      style={{ marginLeft: "30px" }}
+      target="_blank"
+      href="https://hotmart.com/pt-br/marketplace/produtos/arquivos-pre-prontos-para-apresentacoes-tcc-controle-financeiro-curriculo/F83402332H"
+    >
+      <img
+        src={HotmartIMG}
+        alt="Hotmart"
+        style={{
+          height: "40px",
+          width: "auto",
+        }}
+      />
+    </a>
+  );
+};
+
 const Footer = () => {
   return (
     <div
@@ -7,6 +44,9 @@ const Footer = () => {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: "30px",
+        paddingRight: "30px",
       }}
     >
       <p
@@ -15,11 +55,15 @@ const Footer = () => {
           textAlign: "center",
           color: "white",
           fontSize: "26px",
-          marginLeft: "20px",
         }}
       >
         ©️ Todos os direitos reservados para Ordem Financeira
       </p>
+
+      <div>
+        <InstaButton />
+        <HotmartButton />
+      </div>
     </div>
   );
 };
