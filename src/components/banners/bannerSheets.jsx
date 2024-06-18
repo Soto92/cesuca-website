395f218d46018f0fabea0605af7ae050ca168143
Mauricio <mauricioasoto@gmail.com>
2024-06-18 20:39:11 -0300

    implement carousel partially

--- src/components/banners/bannerSheets.jsx
+++ src/components/banners/bannerSheets.jsx
@@ -2,6 +2,23 @@ import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+const CarouselItem = ({ title, text }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#399A52",
+        width: "500px",
+        height: "320px",
+        borderRadius: "10px",
+        padding: "16px",
+      }}
+    >
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </div>
+  );
+};
+
 const BannerSheets = () => {
   const responsive = {
     superLargeDesktop: {
@@ -11,11 +28,11 @@ const BannerSheets = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -23,33 +40,51 @@ const BannerSheets = () => {
     },
   };
 
+  const data = [
+    {
+      title: "DD1",
+      text: "bla bla",
+    },
+    {
+      title: "DD 2",
+      text: "bla bla",
+    },
+    {
+      title: "DD 3",
+      text: "bla bla",
+    },
+    {
+      title: "DD 4",
+      text: "bla bla",
+    },
+    {
+      title: "DD 5",
+      text: "bla bla",
+    },
+    {
+      title: "DD 6",
+      text: "bla bla",
+    },
+  ];
+
   return (
     <div
       style={{
         display: "flex",
         backgroundColor: "#F2F2F2",
-        height: "100px",
         flexDirection: "column",
+        paddingBottom: "30px",
       }}
     >
-      <div>
+      <div style={{ padding: "30px" }}>
         <h2>Nossas Planilhas</h2>
         <p>Veja todos nossos modelos</p>
       </div>
       <div>
-        <Carousel responsive={responsive}>
-          <div style={{ backgroundColor: "green", height: "300px" }}>
-            Item 1
-          </div>
-          <div style={{ backgroundColor: "green", height: "300px" }}>
-            Item 2
-          </div>
-          <div style={{ backgroundColor: "green", height: "300px" }}>
-            Item 3
-          </div>
-          <div style={{ backgroundColor: "green", height: "300px" }}>
-            Item 4
-          </div>
+        <Carousel infinite={true} responsive={responsive}>
+          {data.map((item) => (
+            <CarouselItem {...item} />
+          ))}
         </Carousel>
       </div>
     </div>
